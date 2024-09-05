5f5f14cc8c5b66ba8918fb11f4c07ab89067baa6
Ale-61 <agonzales@powerdata.pe>
2024-09-05 12:54:29 -0500

    Update StreamlitAPP.py

--- StreamlitAPP.py
+++ StreamlitAPP.py
@@ -2,7 +2,7 @@ import pandas as pd
 import altair as alt
 import streamlit as st
 #from snowflake.snowpark.functions import col
-import snowflake-connector-python
+import snowflake.snowpark.functions
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize your Smoothie! :cup_with_straw:")
