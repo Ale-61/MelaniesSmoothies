0e107c7a06b7fe5831a90fde56f239025b87d9af
Ale-61 <agonzales@powerdata.pe>
2024-09-05 11:50:45 -0500

    Update StreamlitAPP.py

--- StreamlitAPP.py
+++ StreamlitAPP.py
@@ -11,3 +11,9 @@ st.write("""Choose the fruits you want in your custom **Smoothie**!"""
 # Client Name
 name_order = st.text_input("Name on Smoothie: ")
 st.write("The current movie title is", name_order)
+
+#Box fruit option
+
+#session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.sesion()
