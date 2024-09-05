8b6ad17ccb34404861ff0ff72d44bb71e46ee0d6
Ale-61 <agonzales@powerdata.pe>
2024-09-05 11:49:21 -0500

    Update StreamlitAPP.py

--- StreamlitAPP.py
+++ StreamlitAPP.py
@@ -3,4 +3,7 @@ import pandas as pd
 import altair as alt
 
 
-st.title('Uber pickups in NYC')
+# Write directly to the app
+st.title(":cup_with_straw: Customize your Smoothie! :cup_with_straw:")
+st.write("""Choose the fruits you want in your custom **Smoothie**!"""
+        )
