2efb5d326e5296523ec07dfad702dffd2111ca5a
Ale-61 <agonzales@powerdata.pe>
2024-09-05 11:30:37 -0500

    Update StreamlitAPP.py

--- StreamlitAPP.py
+++ StreamlitAPP.py
@@ -1,16 +1,6 @@
-# Import python packages
 import streamlit as st
-#from snowflake.snowpark.context import get_active_session
-from snowflake.snowpark.functions import col
+import pandas as pd
+import numpy as np
 
-# Write directly to the app
-st.title(":cup_with_straw: Customize your Smoothie! :cup_with_straw:")
-st.write(
-#    """Replace this example with your own code!
-#    **And if you're new to Streamlit,** check
-#    out our easy-to-follow guides at
-#    [docs.streamlit.io](https://docs.streamlit.io).
-    """Choose the fruits you want in your custom **Smoothie**!
-    """
-)
 
+st.title('Uber pickups in NYC')
